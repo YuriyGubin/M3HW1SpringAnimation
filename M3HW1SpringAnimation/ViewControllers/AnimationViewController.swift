7153644cc0e29d86f391847bb023a1219e786256
YuriyGubin <iosdevelop.2022@gmail.com>
2022-11-04 16:47:53 +0500

    Add final for class

--- M3HW1SpringAnimation/ViewControllers/AnimationViewController.swift
+++ M3HW1SpringAnimation/ViewControllers/AnimationViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SpringAnimation
 
-class AnimationViewController: UIViewController {
+final class AnimationViewController: UIViewController {
     
     @IBOutlet var springView: SpringView!
    
